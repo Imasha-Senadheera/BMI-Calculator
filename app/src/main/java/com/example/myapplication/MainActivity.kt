88f5b3a0ff0401267d8bb987ca00f48c684fbcfe
Imasha Senadheera <imashasenadheera326@gmail.com>
2023-08-25 14:40:18 +0530

    2nd Commit

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -14,5 +14,45 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.weightPicker.minValue = 30
+        binding.weightPicker.maxValue = 150
+
+        binding.heightPicker.minValue = 100
+        binding.heightPicker.maxValue = 250
+
+        binding.weightPicker.setOnValueChangedListener { _, _, _ ->
+            calculateBMI()
+        }
+
+        binding.heightPicker.setOnValueChangedListener { _, _, _ ->
+            calculateBMI()
+        }
+    }
+
+    private fun calculateBMI() {
+        val height = binding.heightPicker.value
+        val doubleHeight = height.toDouble() / 100
+
+        val weight = binding.weightPicker.value
+
+        val bmi = weight.toDouble() / (doubleHeight + doubleHeight)
+
+        binding.resultsTv.text = String.format("Your BMI is : %2f", bmi)
+        binding.healthyTv.text = String.format("Considered : %s", healthyMsg(bmi))
+
+    }
+
+    private fun healthyMsg(bmi: Double): Any? {
+
+        if (bmi < 18.5)
+            return "UnderWeight"
+        if (bmi < 25.0)
+            return "Healthy"
+        if (bmi < 30.0)
+            return "Overweight"
+
+
+        return "Obese"
     }
 }
